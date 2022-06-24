65dcf661278261c2d8bff4ec71058e03296da6f6
CePseudo <40336563+CrazyWolfBE@users.noreply.github.com>
2022-06-24 17:16:45 +0200

    Creating index.php base template

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Informatix</title>
+  <meta name="description" content="Site Web pour l'agence Informatix">
+  <meta name="author" content="Axel">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
